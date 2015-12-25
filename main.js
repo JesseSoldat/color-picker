e6a3d3b82194515428dd0569e8ea454e68ecea04
Jesse Soldat <jsoldat@hotmail.com>
2015-12-25 18:32:42 -0500

    users can pick the number of boxes they want to create if the box is odd it gets one style and another style if it is even

--- main.js
+++ main.js
@@ -8,3 +8,23 @@ function changeColor(c1, c2) {
   document.body.style.color = color2;
 
 }
+var anchor = document.getElementById('anchor');
+
+function pickNumber(number) {
+  var num = document.getElementById(number).value;
+  
+  for (i = 1; i <= num; i++) {
+    console.log(i);
+    var box = document.createElement('div');
+    var text = document.createTextNode('This is box ' + i);
+    box.appendChild(text);
+    anchor.appendChild(box);
+
+    if (i % 2) {
+      box.classList.add("odd");
+    } else {
+      box.classList.add("even");
+    }
+
+  }
+} 
